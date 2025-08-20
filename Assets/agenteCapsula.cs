8e7f4c03cc4c51996dc6444690a79cebc4db6bf1
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-08-20 09:05:25 -0300

    finish

--- Assets/agenteCapsula.cs
+++ Assets/agenteCapsula.cs
@@ -7,15 +7,18 @@ public class agenteCapsula : MonoBehaviour
 {
     [SerializeField] NavMeshAgent tobigerman;
     public Transform bege;
+    [SerializeField] Animator anim;
     // Start is called before the first frame update
     void Awake()
     {
         tobigerman = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         tobigerman.destination = bege.position;
+        anim.SetFloat("Speed", tobigerman.velocity.magnitude);
     }
 }
