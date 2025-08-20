a45a5b9295f5ee8568202dd999663e0aca419981
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-08-20 08:45:33 -0300

    COSAS

---
+++ Assets/agenteCapsula.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class agenteCapsula : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent tobigerman;
+    public Transform bege;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        tobigerman = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tobigerman.destination = bege.position;
+    }
+}
